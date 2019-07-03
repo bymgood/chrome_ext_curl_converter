88d8a4fcaf1ee466eaee486ed6262b765eb48e76
ym.baek <ym.baek@onestorecorp.com>
2019-07-04 00:53:24 +0900

    update to 0.0.0.9

--- js/popup.js
+++ js/popup.js
@@ -4,34 +4,42 @@
 
 'use strict';
 
-const TAG_CLIENT = "ONESTORE_CLIENT:";
-const TAG_SERVICE = "ONESTORE_SERVICE:";
-const HEADER = "#  REQUEST HEADER  #";
-const BODY = "#  REQUEST BODY  #";
+const TAG_CLIENT = 'ONESTORE_CLIENT:';
+const TAG_SERVICE = 'ONESTORE_SERVICE:';
+// const LOG_URL = '#  REQUEST URL  #';
+const LOG_HEADER = '#  REQUEST HEADER  #';
+const LOG_BODY = '#  REQUEST BODY  #';
 
 function getValue(row) {
 	if (row == 'undefined' || row == null) {
-		return "";
+		return '';
 	}
 	
-	const TAG = row.indexOf(TAG_SERVICE) > -1 ? TAG_SERVICE : TAG_CLIENT;
+	let tagServiceIndex = row.indexOf(TAG_SERVICE);
+	let tagClientIndex = row.indexOf(TAG_CLIENT);
 	
-	return row.substr(row.indexOf(TAG)+TAG.length+1, row.length);
+	if (tagServiceIndex == -1 && tagClientIndex == -1) {
+		return '';
+	}
+	
+	const TAG = tagServiceIndex > -1 ? TAG_SERVICE : TAG_CLIENT;
+	
+	return row.substr(row.indexOf(TAG)+TAG.length+1, row.length).trim();
 }
 
 function parseHeader(header) {
-	let result = "";
+	let result = '';
 	$.each(header.split(/], /g), function (index, value) {
-		result += "-H '" + value.replace(/^{|]}/g, '').replace(/=\[/g, ': ') + "' \\\n";
+		result += '-H "' + value.replace(/^{|]}/g, '').replace(/=\[/g, ': ').replace(/["]+/g, '') + '" \\\n';
 	});
 	
 	return result;
 }
 
 function generateCurlCommand(header, body, url, toTxt, isPost, isToFile, isPrettyXml) {
-	let curl = "";
+	let curl = '';
 	// curl += "curl -v --connect-timeout 5 --max-time 10 \\\n";
-	curl += "curl \\\n";
+	curl += 'curl \\\n';
 	curl += parseHeader(header);
 	if (isPost) {
 		curl += appendPostParam(url, body);
@@ -45,24 +53,24 @@ function generateCurlCommand(header, body, url, toTxt, isPost, isToFile, isPrett
 	
 	curl += appendToFile(isToFile);
 	
-	curl += "\n"
+	curl += '\n'
 	toTxt.val(curl);
 	
 	// console.log("CURL command has been generated.");
 }
 
 function appendGetParam(url, body) {
-	let param = "";
-	if (body != "") {
-		param = "?" + body;
+	let param = '';
+	if (body != '') {
+		param = '?' + body;
 	}
-	return "-X GET '" + url + param + "'";
+	return '-X GET "' + url + param + '"';
 }
 
 function appendPostParam(url, body) {
-	let returnVal = "";
-	if (body != "") {
-		returnVal += "-d '" + body + "' \\\n";
+	let returnVal = '';
+	if (body != '') {
+		returnVal += '-d "' + body + '" \\\n';
 	}
 	returnVal += url;
 	
@@ -71,67 +79,67 @@ function appendPostParam(url, body) {
 
 function appendPrettyXml(isPrettyXml) {
 	if (isPrettyXml) {
-		return " | xmllint --format -";
+		return ' | xmllint --format -';
 	} else {
-		return "";
+		return '';
 	}
 }
 
 function appendToFile(isToFile) {
 	if (isToFile) {
-		return " >> response.txt";
+		return ' >> response.txt';
 	} else {
-		return "";
+		return '';
 	}
 }
 
 function copyToClipboard(toTxt) {
-	var $temp = $("<textarea>");
-	$("body").append($temp);	
+	var $temp = $('<textarea>');
+	$('body').append($temp);	
 	$temp.val(toTxt.val()).select();
-	document.execCommand("copy");
+	document.execCommand('copy');
 	$temp.remove();
 	
 	// console.log("CURL command has been copied to clipboard.");
 }
 
 function showInfoPopup() {
-	if ($(".popuptext").is(":visible") == false) {
-		$(".popuptext").fadeIn().delay(1500).fadeOut();
+	if ($('.popuptext').is(':visible') == false) {
+		$('.popuptext').fadeIn().delay(1500).fadeOut();
 	}
 }
 
 $(function () {
-	$("#chk-post, #chk-to-file, #chk-pretty-xml").change(function() {
-		$("#curl-command-parsing").trigger("click");
+	$('#chk-post, #chk-to-file, #chk-pretty-xml').change(function() {
+		$('#curl-command-parsing').trigger('click');
 	});	
 	
-	$("#curl-command-parsing").click(function() {
-		let fromTxt = $("#from-txt");
-		let toTxt = $("#to-txt");
-		let isPost = $("#chk-post").is(":checked"), isToFile = $("#chk-to-file").is(":checked"), isPrettyXml = $("#chk-pretty-xml").is(":checked");
-		toTxt.val("");
+	$('#curl-command-parsing').click(function() {
+		let fromTxt = $('#from-txt');
+		let toTxt = $('#to-txt');
+		let isPost = $('#chk-post').is(':checked'), isToFile = $('#chk-to-file').is(':checked'), isPrettyXml = $('#chk-pretty-xml').is(':checked');
+		toTxt.val('');
 		
 		// console.log("isPost: " + isPost);
 		
-		if (fromTxt.val() == "") {
+		if (fromTxt.val() == '') {
 			return;
 		}
 		
-		let rows = $("#from-txt").val().split("\n");
+		let rows = $('#from-txt').val().split('\n');
 		let headerBegin = false;
-		let url = "";
-		let header = "";
-		let body = "";
+		let url = '';
+		let header = '';
+		let body = '';
 		$.each(rows, function(index, value) {
 			if (value.indexOf(TAG_CLIENT) == -1 && value.indexOf(TAG_SERVICE) == -1) {
 				return;
 			}
 			
-			if (value.indexOf(HEADER) > -1) {
+			if (value.indexOf(LOG_HEADER) > -1) {
 				url = getValue(rows[index-1]);
 				headerBegin = true;
-			} else if (value.indexOf(BODY) > -1) {
+			} else if (value.indexOf(LOG_BODY) > -1) {
 				headerBegin = false;
 				body += getValue(rows[index+1]);
 				return false;
